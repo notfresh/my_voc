f464819fda2df31cba95d8f49fee0037bcc0caf1
zx_ubuntu <1195056983@qq.com>
2018-08-14 23:25:20 +0800

    2018-8-14 word_sentence in passage adjust

--- app/util/word_sentence_in_passage.py
+++ app/util/word_sentence_in_passage.py
@@ -4,6 +4,5 @@ import re
 def word_sentence_in_passage(passage, word):
     result = re.split('[\.|\.\n]', passage)
     result = [item.strip() + '.' for item in result if item]
-    word = ' {}'.format(word)
-    result = [item for item in result if word in item or word+'.' in item]
+    result = [item for item in result if ' {} '.format(word) in item or ' {}.'.format(word) in item]
     return result
