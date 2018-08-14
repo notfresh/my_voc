be26b9fe45ae4a4baa79c4a9ec0f320560ca97de
zx_ubuntu <1195056983@qq.com>
2018-08-14 23:28:18 +0800

    2018-8-14 word_sentence in passage adjust lower sentences

--- app/util/word_sentence_in_passage.py
+++ app/util/word_sentence_in_passage.py
@@ -4,5 +4,5 @@ import re
 def word_sentence_in_passage(passage, word):
     result = re.split('[\.|\.\n]', passage)
     result = [item.strip() + '.' for item in result if item]
-    result = [item for item in result if ' {} '.format(word) in item or ' {}.'.format(word) in item]
+    result = [item for item in result if ' {} '.format(word) in item.lower() or ' {}.'.format(word) in item.lower()]
     return result
