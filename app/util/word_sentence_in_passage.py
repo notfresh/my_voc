efda637f3c597f26d07edd420e68937ecc0f46a2
notfresh_macbookpro <1195056983@qq.com>
2018-10-08 08:59:23 +0800

    2018-10-8 a upgrade to  picking the sentence from the passage

--- app/util/word_sentence_in_passage.py
+++ app/util/word_sentence_in_passage.py
@@ -1,8 +1,18 @@
 import re
 
 
+def next_char(str1, substr1):
+    index = str1.index(substr1) + len(substr1)
+    try:
+        next_char = str1[index]
+    except Exception:
+        return '#'
+    return next_char
+
+
 def word_sentence_in_passage(passage, word):
-    result = re.split('[\.|\.\n]', passage)
-    result = [item.strip() + '.' for item in result if item]
-    result = [item for item in result if ' {} '.format(word) in item.lower() or ' {}.'.format(word) in item.lower()]
+    sentences = re.split('[\.\s|\.\n|\?]', passage)
+    sentences = [item.strip() + '.' for item in sentences if item]
+    # # 这个单词在这个句子中，但是这个单词后面应该跟的是一个标点符号而不是字母，从而排除了这个单词作为另外一个单词一部分的可能。
+    result = [item for item in sentences if ' {}'.format(word) in item.lower() and not next_char(item, word).isalpha()]
     return result
---
+++ mytests/test_function_index.py
@@ -0,0 +1,23 @@
+
+
+str1 = 'a apple applepie'
+substr1 = 'apple'
+index = str1.index(substr1) + len(substr1)
+next_char = str1[index]
+print(index)
+print(next_char)
+print(next_char != ' ')
+print(next_char.isalpha())
+
+
+def next_char(str1, substr1):
+    index = str1.index(substr1) + len(substr1)
+    try:
+        next_char = str1[index]
+    except Exception:
+        return '#'
+    return next_char
+
+
+print('*'*10)
+print(next_char(str1, 'applepie'))
